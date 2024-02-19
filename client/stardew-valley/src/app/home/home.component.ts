378b00741083498ea4228aa00dc9d25b6a2329a3
aramburujulen <aramburu.cajul22@zaragoza.salesianos.edu>
2024-02-19 20:09:41 +0100

    Comienzo de implementacion del juego

--- client/stardew-valley/src/app/home/home.component.ts
+++ client/stardew-valley/src/app/home/home.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 
   randomNpc: any;
+
+  constructor(private api:ApiService){}
+
+  ngOnInit(): void {
+    this.api.getRandomNpc().subscribe(((data: any) => {
+      this.randomNpc = data
+    }))
+  }
 }
