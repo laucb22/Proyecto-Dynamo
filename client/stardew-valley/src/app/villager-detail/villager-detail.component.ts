b864e433820864bd72721af925c84c2893f1482e
laucb22 <castro.bllau22@zaragoza.salesianos.edu>
2024-02-22 19:33:54 +0100

    committtttt

--- client/stardew-valley/src/app/villager-detail/villager-detail.component.ts
+++ client/stardew-valley/src/app/villager-detail/villager-detail.component.ts
@@ -11,6 +11,11 @@ export class VillagerDetailComponent implements OnInit {
   name: any;
   villager: any;
   isDatable = false;
+  day = "";
+  month = "";
+  monthImg = "";
+  genderImg = "";
+  optimismImg = "";
 
   constructor(private route: ActivatedRoute, private apiService: ApiService) {}
 
@@ -20,8 +25,38 @@ export class VillagerDetailComponent implements OnInit {
     this.apiService.getOneNpc(this.name).subscribe(
       data => {
         this.villager = data;
+        // comprobar si es datable o no para mostrar el corazón
         if(this.villager.datable == 'datable'){
           this.isDatable = true;
+        }
+        // comprobar el mes de cumple para mostrar la imagen correspondiente
+        this.month = this.villager.birthday.split(" ")[0];
+        this.day = this.villager.birthday.split(" ")[1];
+        console.log(this.month);
+        if(this.month == "spring"){
+          this.monthImg = "https://stardewvalleywiki.com/mediawiki/images/thumb/9/9c/Spring.png/24px-Spring.png";
+        } else if(this.month == "summer"){
+          this.monthImg = "https://stardewvalleywiki.com/mediawiki/images/thumb/8/85/Summer.png/24px-Summer.png";
+        } else if(this.month == "fall"){
+          this.monthImg = "https://stardewvalleywiki.com/mediawiki/images/thumb/5/5d/Fall.png/24px-Fall.png";
+        } else if(this.month == "winter"){
+          this.monthImg = "https://stardewvalleywiki.com/mediawiki/images/thumb/a/a7/Winter.png/24px-Winter.png";
+        }
+        // comprobar gender
+        if(this.villager.gender == "female"){
+          this.genderImg = "";
+        } else if(this.villager.gender == "male"){
+          this.genderImg = "";
+        } else if(this.villager.gender == "undefined"){
+          this.genderImg = "";
+        }
+        // comprobar optimism
+        if(this.villager.optimism == "positive"){
+          this.optimismImg = "https://stardewcommunitywiki.com/mediawiki/images/1/14/Emote_Happy.png";
+        } else if(this.villager.optimism == "negative"){
+          this.optimismImg = "https://stardewcommunitywiki.com/mediawiki/images/1/18/Emote_Angry.png";
+        } else if(this.villager.optimism == "neutral"){
+          this.optimismImg = "https://stardewcommunitywiki.com/mediawiki/images/0/05/Emote_Uh.gif";
         }
         console.log(this.villager);
       },
