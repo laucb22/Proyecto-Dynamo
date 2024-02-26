08442072d969891ca53d7e5389608469531990ab
laucb22 <castro.bllau22@zaragoza.salesianos.edu>
2024-02-26 20:54:20 +0100

    eliminados componentes edit npc y edit achievement (no se usan)

--- client/stardew-valley/src/app/app-routing.module.ts
+++ client/stardew-valley/src/app/app-routing.module.ts
@@ -2,9 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NpcsComponent } from './npcs/npcs.component';
 import { HomeComponent } from './home/home.component';
-import { EditNpcComponent } from './edit-npc/edit-npc.component';
 import { AchievementsComponent } from './achievements/achievements.component';
-import { EditAchievementComponent } from './edit-achievement/edit-achievement.component';
 import { AddElementComponent } from './add-element/add-element.component';
 import { VillagerDetailComponent } from './villager-detail/villager-detail.component';
 
@@ -12,9 +10,7 @@ const routes: Routes = [
   {path: '', redirectTo: 'home', pathMatch: 'full'},
   {path: 'home', component: HomeComponent},
   {path: 'npcs', component: NpcsComponent},
-  {path: 'edit-npc', component: EditNpcComponent},
   {path: 'achievements', component: AchievementsComponent},
-  {path: 'edit-achievement', component: EditAchievementComponent},
   {path: 'add-element', component: AddElementComponent},
   {path: 'villager-detail/:name', component: VillagerDetailComponent},
 ];
