0ccc57d283f603dbb9de1ce1dd77ea237f948999
laucb22 <castro.bllau22@zaragoza.salesianos.edu>
2024-02-26 17:10:49 +0100

    correccion pipe

--- client/stardew-valley/src/app/custom.displaybeforeearnedpipe.ts
+++ client/stardew-valley/src/app/custom.displaybeforeearnedpipe.ts
@@ -6,9 +6,9 @@ import { Pipe, PipeTransform } from '@angular/core';
         transform(
             value: any,
         ): string | null {
-            if (value == true){
+            if (value == true || value == 'true'){
                 return 'yes'
-            } else if (value == false){
+            } else if (value == false || value == 'false'){
                 return 'no'
             }
             return value 
